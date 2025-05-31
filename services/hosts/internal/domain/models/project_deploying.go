a84a78cba4d3286859777c03f8e87c71495a4842
yymoroz3 <yymoroz3@yandex.ru>
2025-05-31 12:57:12 +0300

    wip

--- services/hosts/internal/domain/models/project_deploying.go
+++ services/hosts/internal/domain/models/project_deploying.go
@@ -1,28 +1,5 @@
 package models
 
-// DeploymentPolicy defines deployment policies for projects
-type DeploymentPolicy string
-
-const (
-	DeploymentPolicyPassthrough            DeploymentPolicy = "PASSTHROUGH"
-	DeploymentPolicyDiskmanager            DeploymentPolicy = "DISKMANAGER"
-	DeploymentPolicyShared                 DeploymentPolicy = "SHARED"
-	DeploymentPolicySharedCryptsetup       DeploymentPolicy = "SHARED_CRYPTSETUP"
-	DeploymentPolicySharedStripedNvme      DeploymentPolicy = "SHARED_STRIPED_NVME"
-	DeploymentPolicySharedLVM              DeploymentPolicy = "SHAREDLVM"
-	DeploymentPolicySharedLVMExtendedPlace DeploymentPolicy = "SHAREDLVM_EXTENDED_PLACE"
-	DeploymentPolicyYTDedicated            DeploymentPolicy = "YT_DEDICATED"
-	DeploymentPolicyYTDedicatedTest        DeploymentPolicy = "YT_DEDICATED_TEST"
-	DeploymentPolicyYTShared               DeploymentPolicy = "YT_SHARED"
-	DeploymentPolicyYTMasters              DeploymentPolicy = "YT_MASTERS"
-	DeploymentPolicyYTMastersK8s           DeploymentPolicy = "YT_MASTERS_K8S"
-	DeploymentPolicyYTStorage              DeploymentPolicy = "YT_STORAGE"
-	DeploymentPolicyYTDefaultShared        DeploymentPolicy = "YT_DEFAULT_SHARED"
-	DeploymentPolicyYTDefaultStorage       DeploymentPolicy = "YT_DEFAULT_STORAGE"
-	DeploymentPolicyYTSacrifice            DeploymentPolicy = "YT_SACRIFICE"
-	DeploymentPolicyMDSDedicated           DeploymentPolicy = "MDS_DEDICATED"
-)
-
 // Secret represents a deployment secret (structure to be defined later)
 type Secret struct {
 	// TODO: Add fields when structure is known
@@ -39,8 +16,11 @@ type ProjectDeploying struct {
 	// Network specifies network configuration
 	Network string `bson:"network"`
 
-	// Policy defines deployment policy
-	Policy DeploymentPolicy `bson:"policy"`
+	// Policy defines deployment policy (possible values: PASSTHROUGH, DISKMANAGER, SHARED,
+	// SHARED_CRYPTSETUP, SHARED_STRIPED_NVME, SHAREDLVM, SHAREDLVM_EXTENDED_PLACE,
+	// YT_DEDICATED, YT_DEDICATED_TEST, YT_SHARED, YT_MASTERS, YT_MASTERS_K8S, YT_STORAGE,
+	// YT_DEFAULT_SHARED, YT_DEFAULT_STORAGE, YT_SACRIFICE, MDS_DEDICATED)
+	Policy string `bson:"policy"`
 
 	// Secrets contains deployment secrets
 	Secrets []Secret `bson:"secrets"`
