8a877585405c9f782897542be14abcf27ca33625
yymoroz3 <yymoroz3@gmail.com>
2026-06-17 14:15:10 +0300

    clear

--- pkg/mediatr/base.go
+++
@@ -1,7 +0,0 @@
-package mediatr
-
-import "context"
-
-type handlerFunc func(ctx context.Context, cmd any) (any, error)
-
-type Middleware = func(ctx context.Context, name string, next func(context.Context) error) error
